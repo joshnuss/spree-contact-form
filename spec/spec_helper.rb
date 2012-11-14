05e41c6ee42d4d58f7657959be99a728e56dc316
Josh Nussbaum <joshnuss@gmail.com>
2012-11-14 16:36:49 -0500

    update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,14 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+# HACK: ugly hack :( to fix syntax issues with vintage syntax. to be removed when upgraded to spree 1.1+
+Factory = FactoryGirl::Syntax::Vintage::Factory
 
 require 'spree_core/testing_support/factories'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
